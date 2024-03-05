a1862e7c3dcc72e4a576d4096f6fa21a58e1c588
Bruno Rodrigo <bnorodrigo@gmail.com>
2024-03-05 14:22:57 +0000

    Get all posts

--- routers/post.py
+++ routers/post.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from routers.schemas import PostBase, PostDisplay
 from db.database import get_db
 from db import db_post
+from typing import List
 
 router = APIRouter(
     prefix='/post',
@@ -21,3 +22,7 @@ def create(request: PostBase, db: Session = Depends(get_db)):
         )
     
     return db_post.create(db, request)
+
+@router.get('/all', response_model=List[PostDisplay])
+def posts(db: Session = Depends(get_db)):
+    return db_post.get_all(db)
